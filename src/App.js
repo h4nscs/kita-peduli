e8484a784529794441de4666e255ad15c441adc4
h4nscs <hans.cs2010@gmail.com>
2025-08-18 14:49:38 +0700

    Perbaikan commit tadi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header";
 import HomePage from "./pages/Home";
 import EventPage from "./pages/Events";
