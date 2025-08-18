a63864ada1b557d833bbe906567aa04e3618f69f
h4nscs <hans.cs2010@gmail.com>
2025-08-18 15:12:40 +0700

    ngetes aja

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -113,7 +113,7 @@ function EventPage() {
           {visibleEvents.map(ev => (
           
           <div key={ev.id} className="eventCard">
-            <img src={process.env.PUBLIC_URL + TES} title={ev.title} className="eventPic"/>
+            <img src={process.env.PUBLIC_URL/EventImages/bersih2.jpg} title={ev.title} className="eventPic"/>
             <div className="cardEventAtas">
             <div className="wrapTeksEvent">
               <h3 className="eventTitle">{ev.title}</h3>
